1d20a33abb5b0c9333ef11b74113dc10934fe2da
Anton Novoselov <nantog@gmail.com>
2021-08-19 20:50:37 +0300

    Add task 27

---
+++ LeetcodePatterns/27. Maximum Depth of Binary Tree.swift
@@ -0,0 +1,34 @@
+//
+//  27. Maximum Depth of Binary Tree.swift
+//  27. Maximum Depth of Binary Tree
+//
+//  Created by Anton Novoselov on 19.08.2021.
+//
+
+import Foundation
+
+// 104. Maximum Depth of Binary Tree
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard let node = root else { return 0 }
+        var maxDepth = 0
+        getMaxDepth(node, 0, &maxDepth)
+        return maxDepth
+    }
+    
+    func getMaxDepth(_ node: TreeNode?, _ depth: Int, _ maxDepth: inout Int) {
+        
+        guard let node = node else {
+            return
+        }
+        
+        if node.left == nil && node.right == nil {
+            maxDepth = max(maxDepth, depth + 1)
+        }
+        
+        getMaxDepth(node.left, depth + 1, &maxDepth)
+        getMaxDepth(node.right, depth + 1, &maxDepth)
+    }
+}
