d8533f14206db923f65150811f8eaef26a7d2892
Anton Novoselov <nantog@gmail.com>
2021-08-23 11:05:41 +0300

    Add task 33

---
+++ LeetcodePatterns/33. Backspace String Compare.swift
@@ -0,0 +1,37 @@
+//
+//  33. Backspace String Compare.swift
+//  33. Backspace String Compare
+//
+//  Created by Anton Novoselov on 23.08.2021.
+//
+
+import Foundation
+
+// 844. Backspace String Compare
+// https://leetcode.com/problems/backspace-string-compare/
+
+class Solution844 {
+    func backspaceCompare(_ s: String, _ t: String) -> Bool {
+        
+        var stack1 = [Character]()
+        var stack2 = [Character]()
+        
+        for letter in s {
+            if letter == "#" {
+                stack1.popLast()
+            } else {
+                stack1.append(letter)
+            }
+        }
+        
+        for letter in t {
+            if letter == "#" {
+                stack2.popLast()
+            } else {
+                stack2.append(letter)
+            }
+        }
+        
+        return stack1 == stack2
+    }
+}
