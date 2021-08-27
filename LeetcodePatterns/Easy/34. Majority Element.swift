806be6f41b886110141d13ad01e9e6cbbe79442e
Anton Novoselov <nantog@gmail.com>
2021-08-27 23:52:27 +0300

    Add O(1) space solution for Task 34

--- LeetcodePatterns/Easy/34. Majority Element.swift
+++ LeetcodePatterns/Easy/34. Majority Element.swift
@@ -10,19 +10,43 @@ import Foundation
 // 169. Majority Element
 // https://leetcode.com/problems/majority-element/
 
+// O(n) space
+//class Solution169 {
+//    func majorityElement(_ nums: [Int]) -> Int {
+//        var countMap = [Int:Int]()
+//        for num in nums{
+//            countMap[num, default: 0] += 1
+//        }
+//        
+//        for key in countMap.keys{
+//            if let count = countMap[key], count > nums.count/2{
+//                return key
+//            }
+//        }
+//        
+//        return -1
+//    }
+//}
+
+// O(1) space
 class Solution169 {
     func majorityElement(_ nums: [Int]) -> Int {
-        var countMap = [Int:Int]()
-        for num in nums{
-            countMap[num, default: 0] += 1
-        }
+        var candidate: Int?
         
-        for key in countMap.keys{
-            if let count = countMap[key], count > nums.count/2{
-                return key
+        var vote = 0
+        
+        for num in nums {
+            if vote == 0 {
+                candidate = num
+            }
+            
+            if num == candidate {
+                vote += 1
+            } else {
+                vote -= 1
             }
         }
         
-        return -1
+        return candidate ?? 0
     }
 }
