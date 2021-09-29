94803a3b03e12ec5bf18b222565c426de72b16ae
Anton Novoselov <nantog@gmail.com>
2021-09-29 12:55:17 +0300

    Add task 40

---
+++ LeetcodePatterns/Medium/40. Spiral Matrix.swift
@@ -0,0 +1,68 @@
+//
+//  40. Spiral Matrix.swift
+//  LeetcodePatterns
+//
+//  Created by Anton Novoselov on 29.09.2021.
+//
+
+import Foundation
+
+// 54. Spiral Matrix
+// https://leetcode.com/problems/spiral-matrix/
+
+class Solution54 {
+    
+    enum Direction {
+        case right
+        case down
+        case left
+        case up
+    }
+    
+    func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+        
+        var result: [Int] = []
+        
+        var top = 0
+        var bottom = matrix.count - 1
+        
+        var left = 0
+        var right = matrix[0].count - 1
+        
+        var direction: Direction = .right
+        
+        while top <= bottom && left <= right {
+            switch direction {
+            case .right:
+                for i in left ... right {
+                    result.append(matrix[top][i])
+                }
+                top += 1
+                direction = .down
+            
+            case .down:
+                for i in top ... bottom {
+                    result.append(matrix[i][right])
+                }
+                right -= 1
+                direction = .left
+            
+            case .left:
+                for i in stride(from: right, through: left, by: -1) {
+                    result.append(matrix[bottom][i])
+                }
+                bottom -= 1
+                direction = .up
+            
+            case .up:
+                for i in stride(from: bottom, through: top, by: -1) {
+                    result.append(matrix[i][left])
+                }
+                left += 1
+                direction = .right
+            }
+        }
+        
+        return result
+    }
+}
