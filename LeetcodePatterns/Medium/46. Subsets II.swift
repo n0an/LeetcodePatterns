39c13b7680ef0bac01a4f86f2b372bd1332ae72b
Anton Novoselov <nantog@gmail.com>
2021-10-03 11:05:03 +0300

    Add task 46

---
+++ LeetcodePatterns/Medium/46. Subsets II.swift
@@ -0,0 +1,29 @@
+//
+//  46. Subsets II.swift
+//  LeetcodePatterns
+//
+//  Created by Anton Novoselov on 03.10.2021.
+//
+
+import Foundation
+
+// 90. Subsets II
+// https://leetcode.com/problems/subsets-ii/
+
+class Solution90 {
+    func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+        result.append([])
+        var count = result.count
+        var nums = nums.sorted() // Simplifies skipping duplicates
+
+        for (i, num) in nums.enumerated() {
+            let start = (i > 0 && nums[i-1] == num) ? count : 0
+            count = result.count
+            for j in start ..< count {
+                result.append( result[j] + [num] )
+            }
+        }
+        return result
+    }
+}
