9742140949c76a73c108186e042ee7ec5cbd93d0
Anton Novoselov <nantog@gmail.com>
2021-10-03 12:13:58 +0300

    Update

---
+++ LeetcodePatterns/Medium/47. Permutations.swift
@@ -0,0 +1,36 @@
+//
+//  47. Permutations.swift
+//  LeetcodePatterns
+//
+//  Created by Anton Novoselov on 03.10.2021.
+//
+
+import Foundation
+
+// 46. Permutations
+// https://leetcode.com/problems/permutations/
+
+class Solution46 {
+    var result: [[Int]]!
+    
+    func permute(_ nums: [Int]) -> [[Int]] {
+        self.result = []
+        var nums = nums
+        self.backtrack(0, &nums)
+        
+        return self.result
+    }
+    
+    func backtrack(_ current: Int, _ nums: inout [Int]) {
+        if current == nums.count {
+            self.result.append(nums)
+            return
+        }
+        
+        for i in current ..< nums.count {
+            nums.swapAt(i, current)
+            self.backtrack(current + 1, &nums)
+            nums.swapAt(i, current)
+        }
+    }
+}
